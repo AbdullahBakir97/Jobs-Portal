f9daa48e66a6b3cbd2f9b3a753760591c2c329e8
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2023-11-08 05:48:19 +0100

    add Job Admin customization

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Job , Category , Company
 
+class JobAdmin(admin.ModelAdmin):
+    list_display=['title','category','agency','salary','location']
+    list_filter=['job_nature','category','agency']
+    search_fields=['title','nature','salary','agency']
 
-admin.site.register(Job)
 
+admin.site.register(Job,JobAdmin)
 admin.site.register(Category)
-
 admin.site.register(Company)
--- jobs/models.py
+++ jobs/models.py
@@ -40,7 +40,7 @@ class Category(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
+        self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
 class Company(models.Model):
@@ -55,5 +55,8 @@ class Company(models.Model):
         return self.name
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)   
+        self.slug = slugify(self.name)   
         super(Company, self).save(*args, **kwargs)
+
+
+        
