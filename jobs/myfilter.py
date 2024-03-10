0b84bfba3c246410a1c2d9e3bb1ba985c7e09745
Abdullah Bakir <abdullah.bakir.204@gmail.com>
2024-03-10 03:55:22 +0100

    edit views & filters

--- jobs/myfilter.py
+++ jobs/myfilter.py
@@ -1,16 +1,33 @@
+import django_filters
 from django_filters import rest_framework as filters
-from .models import Job
+from .models import Job, Category, Company
 
+class JobFilter(django_filters.FilterSet):
+    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
+    salary__lte = django_filters.NumberFilter(field_name='salary', lookup_expr='lte')
+    salary__gte = django_filters.NumberFilter(field_name='salary', lookup_expr='gte')
+    job_nature = django_filters.ChoiceFilter(field_name='job_nature')
+    agency = django_filters.CharFilter(field_name='agency__name', lookup_expr='icontains')
+    vacancy = django_filters.NumberFilter(field_name='vacancy', lookup_expr='exact')
 
-
-class JobFilter(filters.FilterSet):
     class Meta:
         model = Job
-        fields = {
-            'title': ['contains', ],
-            'salary': ['lte', 'gte', 'range'],
-            'job_nature': ['exact'],
-            'agency': ['exact'],
-            'vacancy': ['exact'],
-            
-        }
+        fields = []
+
+class CategoryFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    job_count__lte = django_filters.NumberFilter(field_name='job_count', lookup_expr='lte')
+    job_count__gte = django_filters.NumberFilter(field_name='job_count', lookup_expr='gte')
+
+    class Meta:
+        model = Category
+        fields = []
+
+class CompanyFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    email = django_filters.CharFilter(field_name='email', lookup_expr='icontains')
+
+    class Meta:
+        model = Company
+        fields = []
+
--- jobs/views.py
+++ jobs/views.py
@@ -9,86 +9,66 @@ from .models import Job, Category, Company
 from .forms import JobForm, CategoryForm, CompanyForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 
 
-class JobList(generic.ListView):
+
+# Job Views
+class JobList(ListView):
     model = Job
     template_name = 'jobs/job_list.html'
 
-class JobDetail(generic.DetailView):
+class JobDetail(DetailView):
     model = Job  
     template_name = 'jobs/job_detail.html'
 
-class JobCreate(generic.CreateView):
+class JobCreate(LoginRequiredMixin, CreateView):
     model = Job
     form_class = JobForm
     template_name = 'jobs/add_job.html'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        if form.is_valid():
-            print("Form is valid. Saving data...")
-        else:
-            print("Form is invalid. Errors:", form.errors)
         return super().form_valid(form)
-  
 
-
-class JobUpdate(UpdateView):
+class JobUpdate(LoginRequiredMixin, UpdateView):
     model = Job
     form_class = JobForm
     template_name = 'jobs/edit_job.html'
-    success_url = '/jobs/'
+    success_url = reverse_lazy('job_list')
 
-
-class JobDelete(DeleteView):
+class JobDelete(LoginRequiredMixin, DeleteView):
     model = Job
     template_name = 'jobs/delete.html'
-    success_url = reverse_lazy('delete_confirm')
-
-    def get_object(self, queryset=None):
-        return get_object_or_404(Job, slug=self.kwargs['job_slug'])
-
-class JobDeleteConfirm(DeleteView):
-    model = Job
-    template_name = 'jobs/delete_confirm.html'
     success_url = reverse_lazy('job_list')
 
     def get_object(self, queryset=None):
         return get_object_or_404(Job, slug=self.kwargs['job_slug'])
 
-    def delete(self, request, *args, **kwargs):
-        job = self.get_object()
-        job.delete()
-        return HttpResponseRedirect(self.success_url)
-
-
-
 # Category Views
-
-class CategoryList(generic.ListView):
+class CategoryList(ListView):
     model = Category
     template_name = 'jobs/category_list.html'
 
-class CategoryDetail(generic.DetailView):
+class CategoryDetail(DetailView):
     model = Category
     template_name = 'jobs/category_detail.html'
 
-class CategoryCreate(generic.CreateView):
+class CategoryCreate(LoginRequiredMixin, CreateView):
     model = Category
     form_class = CategoryForm
     template_name = 'jobs/add_category.html'
     success_url = reverse_lazy('category_list')
 
-class CategoryUpdate(UpdateView):
+class CategoryUpdate(LoginRequiredMixin, UpdateView):
     model = Category
     form_class = CategoryForm
     template_name = 'jobs/edit_category.html'
     success_url = reverse_lazy('category_list')
 
-class CategoryDelete(DeleteView):
+class CategoryDelete(LoginRequiredMixin, DeleteView):
     model = Category
     template_name = 'jobs/delete_category.html'
     success_url = reverse_lazy('category_list')
@@ -97,33 +77,30 @@ class CategoryDelete(DeleteView):
         return get_object_or_404(Category, slug=self.kwargs['slug'])
 
 # Company Views
-
-class CompanyList(generic.ListView):
+class CompanyList(ListView):
     model = Company
     template_name = 'jobs/company_list.html'
 
-class CompanyDetail(generic.DetailView):
+class CompanyDetail(DetailView):
     model = Company
     template_name = 'jobs/company_detail.html'
 
-class CompanyCreate(generic.CreateView):
+class CompanyCreate(LoginRequiredMixin, CreateView):
     model = Company
     form_class = CompanyForm
     template_name = 'jobs/add_company.html'
     success_url = reverse_lazy('company_list')
 
-class CompanyUpdate(UpdateView):
+class CompanyUpdate(LoginRequiredMixin, UpdateView):
     model = Company
     form_class = CompanyForm
     template_name = 'jobs/edit_company.html'
     success_url = reverse_lazy('company_list')
 
-class CompanyDelete(DeleteView):
+class CompanyDelete(LoginRequiredMixin, DeleteView):
     model = Company
     template_name = 'jobs/delete_company.html'
     success_url = reverse_lazy('company_list')
 
     def get_object(self, queryset=None):
         return get_object_or_404(Company, slug=self.kwargs['slug'])
-    
-    
